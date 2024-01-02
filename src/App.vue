d6c619a6c1064f3b1aa80c1578af0b14766f5179
LucaZH <lucarandria1@gmail.com>
2024-01-02 06:29:20 +0300

    add typing record

--- src/App.vue
+++ src/App.vue
@@ -3,98 +3,54 @@
     <div class="calc-body">
       <div class="calc-screen">
         <div class="calc-operation">2536 + 419 + </div>
-        <div class="calc-typed">2955</div>
+        <div class="calc-typed">{{ typedLabel }}</div>
       </div>
       <div class="calc-button-row">
-        <Btn class="c" label="1" />
-        <Btn class="l" label="≠" />
-        <Btn class="l" label="%" />
-        <Btn class="l" label="/" />
+        <Btn class="c" label="1" v-model="typedLabel"/>
+        <Btn class="l" label="≠" v-model="typedLabel"/>
+        <Btn class="l" label="%" v-model="typedLabel"/>
+        <Btn class="l" label="/" v-model="typedLabel"/>
       </div>
       <div class="calc-button-row">
-        <Btn label="7" />
-        <Btn label="8" />
-        <Btn label="9" />
-        <Btn class="l" label="x" />
+        <Btn label="7" v-model="typedLabel"/>
+        <Btn label="8" v-model="typedLabel"/>
+        <Btn label="9" v-model="typedLabel"/>
+        <Btn class="l" label="x" v-model="typedLabel"/>
       </div>
       <div class="calc-button-row">
-        <Btn label="4" />
-        <Btn label="5" />
-        <Btn label="6" />
-        <Btn class="l" label="−" />
+        <Btn label="4" v-model="typedLabel"/>
+        <Btn label="5" v-model="typedLabel"/>
+        <Btn label="6" v-model="typedLabel"/>
+        <Btn class="l" label="−" v-model="typedLabel"/>
       </div>
       <div class="calc-button-row">
-        <Btn label="1" />
-        <Btn label="2" />
-        <Btn label="3" />
-        <Btn class="l" label="+" />
+        <Btn label="1" v-model="typedLabel"/>
+        <Btn label="2" v-model="typedLabel"/>
+        <Btn label="3" v-model="typedLabel"/>
+        <Btn class="l" label="+" v-model="typedLabel"/>
       </div>
       <div class="calc-button-row">
-        <Btn label="." />
-        <Btn label="0" />
-        <Btn class="l" label="=" />
+        <Btn label="." v-model="typedLabel"/>
+        <Btn label="0" v-model="typedLabel"/>
+        <Btn class="l" label="="/>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import Btn from './components/Btn.vue';
 
 export default defineComponent({
   components: {
     Btn,
   },
+  setup(){
+    const typedLabel = ref('');
+    
+    return { typedLabel, }
+  }
 });
 </script>
-<style scoped>
-* {
-  box-sizing: border-box;
-  font-family: 'Roboto', sans-serif;
-}
 
-body {
-  background: #EAEBEC;
-}
-
-.container {
-  width: 400px;
-  margin: auto;
-}
-
-.calc-body {
-  width: 275px;
-  margin: auto;
-  min-height: 400px;
-  border: solid 1px #3A4655;
-  box-shadow: 0 8px 50px -7px black;
-}
-
-.calc-screen {
-  background: #3A4655;
-  width: 100%;
-  height: 150px;
-  padding: 20px;
-}
-
-.calc-operation {
-  text-align: right;
-  color: #727B86;
-  font-size: 21px;
-  padding-bottom: 10px;
-  border-bottom: dotted 1px;
-}
-
-.calc-typed {
-  margin-top: 20px;
-  font-size: 45px;
-  text-align: right;
-  color: #fff;
-}
-
-.calc-button-row {
-  width: 100%;
-  background: #3C4857;
-}
-</style>
--- src/components/Btn.vue
+++ src/components/Btn.vue
@@ -3,19 +3,25 @@
   </template>
   
   <script lang="ts">
-  import { defineComponent, PropType } from 'vue';
+  import { defineComponent } from 'vue';
   
   export default defineComponent({
     name: 'CalcButton',
     props: {
       label: {
-        type: String, 
+        type: [String, Number],
         required: true,
       },
+      modelValue: {
+        type: String,
+        default: '',
+      },
     },
-    setup(props) {
+    emits: ['update:modelValue'],
+    setup(props, { emit }) {
       const handleClick = () => {
         console.log(`Button "${props.label}" clicked!`);
+        emit('update:modelValue', props.modelValue + props.label);
       };
   
       return {
@@ -24,23 +30,25 @@
     },
   });
   </script>
-<style scoped>
-.button {
-  width: 25%;
-  background: #425062;
-  color: #fff;
-  padding: 20px;
-  display: inline-block;
-  font-size: 25px;
-  text-align: center;
-  vertical-align: middle;
-  margin-right: -4px;
-  border-right: solid 2px #3C4857;
-  border-bottom: solid 2px #3C4857;
-  transition: all 0.2s ease-in-out;
-}
-.button:hover {
-  background: #E0B612;
-  transform: rotate(1deg);
-}
-</style>
+  
+  <style scoped>
+  .button {
+    width: 25%;
+    background: #425062;
+    color: #fff;
+    padding: 20px;
+    display: inline-block;
+    font-size: 25px;
+    text-align: center;
+    vertical-align: middle;
+    margin-right: -4px;
+    border-right: solid 2px #3C4857;
+    border-bottom: solid 2px #3C4857;
+    transition: all 0.2s ease-in-out;
+  }
+  .button:hover {
+    background: #E0B612;
+    transform: rotate(1deg);
+  }
+  </style>
+  
